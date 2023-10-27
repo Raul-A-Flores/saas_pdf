ab978544ff9646dc3ac93c15e4eb1d531feb8fd3
Raul-A-Flores <raul.alexander.floress@gmail.com>
2023-10-27 14:23:24 -0700

    Pinecone integrated with openai

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -29,10 +29,11 @@ export async function POST(req: Request){
         const lastMessage = messages[messages.length -1]
         console.log(lastMessage)
         const context = await getContext(lastMessage.content, fileKey)
+        console.log(context)
 
 
         const prompt = {
-            /* role: "system",
+            role: "system",
             content: `AI assistant is a brand new, powerful, human-like artificial intelligence.
             The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
             AI is a well-behaved and well-mannered individual.
@@ -40,23 +41,23 @@ export async function POST(req: Request){
             AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
             AI assistant is a big fan of Pinecone and Vercel.
             START CONTEXT BLOCK
+            
             ${context}
             END OF CONTEXT BLOCK
             AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
             If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
             AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
             AI assistant will not invent anything that is not drawn directly from the context.
-            `, */
+            `,
           };
 
 
         const response = await openai.createChatCompletion({
 
             model: 'gpt-3.5-turbo',
-           /*  messages:{
+            messages:[
                 prompt, ...messages.filter((message: Message) => messages.role === 'user')
-            }, */
-            messages,
+            ],
             stream: true
         })
 
