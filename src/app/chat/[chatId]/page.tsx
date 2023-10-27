a50ee4fe278866d4d71ed5079a823c87a0e81337
Raul-A-Flores <raul.alexander.floress@gmail.com>
2023-10-27 13:41:38 -0700

    OpenAI API enabled

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -6,6 +6,7 @@ import React from 'react'
 import { eq } from 'drizzle-orm';
 import ChatSideBar from '@/components/ChatSideBar';
 import PDFViewer from '@/components/PDFViewer';
+import ChatComponent from '@/components/ChatComponent';
 
 type Props = {
 
@@ -32,18 +33,19 @@ const ChatPage = async ({params: { chatId }}: Props) => {
 
     const currentChat = _chats.find(chat => chat.id ===parseInt(chatId))
   return (
-    <div className='flex max-h-screen overflow-scroll'>
-        <div className='flex w-full max-h-screen overflow-scroll'>
+    <div className='flex max-h-screen'>
+       
             <div className='flex-[1] max-w-xs'>
                 <ChatSideBar chats={_chats} chatId={parseInt(chatId)}/>
             </div>
-            <div className='mex-h-screen p-4 overflow-scroll flex-[5]'>
+            <div className='max-h-screen p-4 flex-[5]'>
                 <PDFViewer pdf_url={currentChat?.pdfUrl || '' }/>
             </div>
             <div className='flex-[3] border-1-4 border-1-slate-200'>
+                <ChatComponent />
             </div>
 
-        </div>
+
     </div>
   )
 }
