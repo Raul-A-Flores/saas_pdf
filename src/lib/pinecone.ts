24577e773631c8b33ffe92a8c6922275f5e26229
Raul-A-Flores <raul.alexander.floress@gmail.com>
2023-12-06 20:21:16 -0800

    metadata

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -5,6 +5,7 @@ import {Document, RecursiveCharacterTextSplitter} from '@pinecone-database/doc-s
 import { getEmbeddings } from './embeddings';
 import md5 from 'md5'
 import { convertToAscii } from './utils';
+import { randomInt } from 'crypto';
 
 export const getPineconeClient = () => {
     return new Pinecone({
@@ -17,29 +18,29 @@ export const getPineconeClient = () => {
   type PDFPage = {
     pageContent: string;
     metadata: {
-      loc: { pageNumber: number };
+      loc: { pageNumber: number};
+      key: string;
     };
   };
+
+
+
 
 export async function loadS3IntoPinecone(fileKey: string){
   
   const file_name = await downloadFromS3(fileKey);
+  console.log(fileKey)
   console.log("downloading s3 into file system")
-
-    
 if(!file_name){
     throw new Error('could not download from s3')
 }
-
  const loader = new PDFLoader(file_name);
  const pages = (await loader.load()) as PDFPage[];
  // console.log(pages)
-
   const documents = await Promise.all(pages.map(prepareDocument))
 
 
   // Vectorize and embed individual documents
-
   const vectors = await Promise.all(documents.flat().map(embedDocument))
 
   // upload to pinecone
@@ -49,31 +50,32 @@ if(!file_name){
 
   console.log('inserting vectors into pinecone')
 
+
   await pineconeIndex.upsert(vectors);
+
 
   /* const namespace = pineconeIndex.namespace(convertToAscii(fileKey));
 
   await namespace.upsert(vectors); */
-
   return documents[0];
 }
-
 
   // Vectorizing and embedding documents 
 
 async function embedDocument(doc:Document){
-  try {
-
+  
+    try {
     const embeddings = await getEmbeddings(doc.pageContent)
     const hash = md5(doc.pageContent)
 
     return{
-
         id: hash, 
         values: embeddings, 
         metadata: {
           text:doc.metadata.text,
-          pageNumber: doc.metadata.pageNumber
+          pageNumber: doc.metadata.pageNumber,
+          key: " "
+          
         }
     } as PineconeRecord ;
     
@@ -91,17 +93,22 @@ export const truncateStringByBytes = (str: string, bytes: number) => {
   return new TextDecoder('utf-8').decode(enc.encode(str).slice(0,bytes))
 }
 
-async function prepareDocument(page: PDFPage){
-    let {pageContent, metadata } = page
-    pageContent = pageContent.replace(/\n/g, '') 
 
+
+async function prepareDocument(page: PDFPage){
+
+    let {pageContent, metadata } = page;
+    const hash = randomInt(0,2000)
+
+    pageContent = pageContent.replace(/\n/g, '') 
     const splitter = new RecursiveCharacterTextSplitter()
     const docs = await splitter.splitDocuments([
       new Document({
         pageContent, 
         metadata: {
           pageNumber: metadata.loc.pageNumber,
-          text: truncateStringByBytes(pageContent, 36000)
+          text: truncateStringByBytes(pageContent, 36000),
+          key: ''
         }
       })
     ])
